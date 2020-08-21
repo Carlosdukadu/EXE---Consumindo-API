1c50d09c7f82fd0f305243ca9fe58b0b105dbc9d
Carlosdukadu <carlos.du.kadud@gmail.com>
2020-08-20 22:36:31 -0300

    console.log da API funcionando

--- js/main.js
+++ js/main.js
@@ -1,36 +1,60 @@
 
-var requestURL = 'https://jsonplaceholder.typicode.com/users'
+// var requestURL = 'https://jsonplaceholder.typicode.com/users'
 
-var request = new XMLHttpRequest();
+// var request = new XMLHttpRequest();
 
-request.open('GET', requestURL);
+// request.open('GET', requestURL);
 
-request.responseType = 'json';
-request.send();
-request.onload = function(){
-    var contatos = request.response;
-    populateHeader(contatos);
-    showContatos(contatos);
+// request.responseType = 'json';
+// request.send();
+// request.onload = function(){
+//     var contatos = request.response;
+//     populateHeader(contatos);
+//     showContatos(contatos);
+// }
+
+// function populateHeader(jsonObj){
+//     var contato1 = document.createElement('C1');
+//     contato1.textContent = jsonObj['squadName'];
+//     header.appendChild(contato1);
+
+//     var contatoPara = document.createElement('p');
+//     contatoPara.textContent = 'Home:' + jsonObj['homeTown'] + '//Formed: ' + jsonObj['formed'];
+//     header.appendChild(contatoPara);
+// }
+
+
+// function showContatos(jsonObj){
+//     var _showContatos = jsonObj['menbers'];
+
+//     for (var i = 0; i < _showContatos.length; i++ ){
+//         var 
+//     }
+// }
+
+var url = 'https://jsonplaceholder.typicode.com/users';
+
+var botao = document.getElementById('btnLupa');
+console.log(botao, '---------------- variavel BOTAO')
+
+
+botao.onclick = function receberApi(){
+fetch(url)
+// console.log(url, '---------------- variavel url')
+.then(response => response.json())
+.then(json =>{ 
+    console.log(JSON.stringify(json), 'json dentro do fetch')
+    // return JSON.stringify(json);
+    
+    var myJson = json;
+    console.log(url, '---------------- variavel url')
+    console.log(JSON.stringify(myJson), '-+-+-+--+-+-+-+-+-- variavel myJson')
+})
+
+
+
+
+
+
 }
 
-function populateHeader(jsonObj){
-    var contato1 = document.createElement('C1');
-    contato1.textContent = jsonObj['squadName'];
-    header.appendChild(contato1);
-
-    var contatoPara = document.createElement('p');
-    contatoPara.textContent = 'Home:' + jsonObj['homeTown'] + '//Formed: ' + jsonObj['formed'];
-    header.appendChild(contatoPara);
-}
-
-
-function showContatos(jsonObj){
-    var _showContatos = jsonObj['menbers'];
-
-    for (var i = 0; i < _showContatos.length; i++ ){
-        var 
-    }
-}
-// fetch('https://jsonplaceholder.typicode.com/users')
-//   .then(response => response.json())
-//   .then(json => console.log(json))
