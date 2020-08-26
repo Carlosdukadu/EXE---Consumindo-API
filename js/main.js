2eb05de7447fb0982d589910c8360a0c5eb73e99
Carlos Eduardo Santos Teixeira <CETeixeira@rd.com.br>
2020-08-26 17:03:58 -0300

    funcionando para mostrar contatos na tela e validção de input vazio

--- js/main.js
+++ js/main.js
@@ -1,60 +1,93 @@
+let url = "https://jsonplaceholder.typicode.com/users";
+let _labelInput = document.getElementById("labelInput");
+let _urlDigitada = document.getElementById("urlDigitada");
+let botao = document.getElementById("btnLupa");
+// console.log(botao, "---------------- variavel BOTAO");
+const containerEl = document.querySelector(".container");
+// var json = [];
 
-// var requestURL = 'https://jsonplaceholder.typicode.com/users'
-
-// var request = new XMLHttpRequest();
-
-// request.open('GET', requestURL);
-
-// request.responseType = 'json';
-// request.send();
-// request.onload = function(){
-//     var contatos = request.response;
-//     populateHeader(contatos);
-//     showContatos(contatos);
-// }
-
-// function populateHeader(jsonObj){
-//     var contato1 = document.createElement('C1');
-//     contato1.textContent = jsonObj['squadName'];
-//     header.appendChild(contato1);
-
-//     var contatoPara = document.createElement('p');
-//     contatoPara.textContent = 'Home:' + jsonObj['homeTown'] + '//Formed: ' + jsonObj['formed'];
-//     header.appendChild(contatoPara);
-// }
-
-
-// function showContatos(jsonObj){
-//     var _showContatos = jsonObj['menbers'];
-
-//     for (var i = 0; i < _showContatos.length; i++ ){
-//         var 
-//     }
-// }
-
-var url = 'https://jsonplaceholder.typicode.com/users';
-
-var botao = document.getElementById('btnLupa');
-console.log(botao, '---------------- variavel BOTAO')
-
-
-botao.onclick = function receberApi(){
-fetch(url)
-// console.log(url, '---------------- variavel url')
-.then(response => response.json())
-.then(json =>{ 
-    console.log(JSON.stringify(json), 'json dentro do fetch')
-    // return JSON.stringify(json);
-    
-    var myJson = json;
-    console.log(url, '---------------- variavel url')
-    console.log(JSON.stringify(myJson), '-+-+-+--+-+-+-+-+-- variavel myJson')
-})
-
-
-
-
-
-
+/************************************ MÉTODO PARA GERAR CARDS **********************************/
+/************************************************************************************************/
+function gerarCards(myJson) {
+  let html = ``;
+  let _myson = myJson;
+  // myJson = JSON.stringify(myJson)
+  _myson.forEach((item) => {
+    html +=
+      '<div class="card-container">' +
+      `<h3 class="card-name card-id"> Nome: ${item.name} </h3>` +
+      `<p card-email> e-mail:  ${item.email} </p>` +
+      `<p card-username> Nome de usuário:  ${item.username} </p>` +
+      "</div>";
+  });
+  this.enviarDadosParaHtml(html);
 }
 
+/************************* MÉTODOS PARA ENVIAR CARDS PARA HTML ******************************/
+/********************************************************************************************/
+function enviarDadosParaHtml(a) {
+  containerEl.innerHTML = a; // insere o conteúdo na div container do HTML
+}
+
+/************************* MÉTODOS PARA RECEBER PROCESSAR DADOS DA API ******************************/
+/****************************************************************************************************/
+function gerarURL() {
+  if (_urlDigitada.value === "") {
+    metodoVerificarVazio();
+
+    console.log(
+      _urlDigitada.value,
+      "---------CONSOLE.LOG URL DIGITADA-------------"
+    );
+    // window.alert("Api não inserida ou API invalida correspondente!");
+    return;
+  } else {
+    fetch(_urlDigitada.value)
+      // console.log(url, '---------------- variavel url')
+      .then((response) => response.json())
+      .then((json) => {
+        // myJson = json;
+        console.log(JSON.stringify(json), "json dentro do fetch");
+        console.log(json, "json dentro do fetch2");
+
+        //CHAMADA DO MÉTODO GERAR CARDS//
+        console.log(json.length, "----------myJson ---------");
+        this.gerarCards(json);
+      });
+  }
+}
+
+/************** PEGAR CLICK E CHAMAR FUNÇÃO GERAR URL ********************/
+/*************************************************************************/
+botao.addEventListener("click", (e) => {
+  console.log(e, "-----click addEventListener----");
+  e.stopPropagation;
+  this.gerarURL();
+});
+
+function metodoVerificarVazio() {
+  console.log("entrei metodo erro");
+  _urlDigitada.classList.add("msgErrorCampoUrlVazio");
+  _urlDigitada.placeholder = "Campo obrigatório";
+  _labelInput.style.display = "block";
+}
+
+/*
+        _urlDigitada.addEventListener('keyup', function(e){
+            const inputUrl = e.target;
+            const valueInputUrl = e.target.value;
+
+            if(valueInputUrl.length == url){
+                inputUrl.classList.remove('--has-error');
+            } else {
+                inputUrl.classList.add('--has-error');
+            }
+        })
+
+*/
+
+// botao.onclick = this._gerarCards;
+// this.gerarCards();
+// return JSON.stringify(json);
+// this.gerarCards();
+//  console.log('Clik no botão para comparar e consultar URL');
